906e01d4afb871518ab9f25131793395c8ea8dde
Callum Irving <callum@callumirving.com>
2024-01-22 10:30:17 -0500

    fix merge conflict

--- lib/p11.ml
+++ lib/p11.ml
@@ -9,7 +9,7 @@ let encode lst =
     | x :: (y :: _ as tl) ->
         if x = y then helper (count + 1) acc tl
         else helper 0 (make_rle count x :: acc) tl
-    | x :: [] -> if count = 0 then One x :: acc else Many (count + 1, x) :: acc
+    | x :: [] -> (make_rle count x) :: acc
     | [] -> acc
   in
   List.rev (helper 0 [] lst)
