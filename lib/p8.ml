422703ce40277049ce91b95f78352854393feac2
Callum Irving <callum@callumirving.com>
2024-01-21 22:37:53 -0500

    update problem 8 solution

--- lib/p8.ml
+++ lib/p8.ml
@@ -1,6 +1,5 @@
 let rec compress lst =
   match lst with
-  | [] -> []
   | x :: y :: rest ->
       if x = y then compress (y :: rest) else x :: compress (y :: rest)
-  | x :: rest -> x :: compress rest
+  | lst -> lst
