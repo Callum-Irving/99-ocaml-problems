6ccab0cb51ed2933ddfae42806e9dc1fabe0a86e
Callum Irving <callum@callumirving.com>
2024-01-22 10:26:30 -0500

    update problem 11 solution

--- lib/p11.ml
+++ lib/p11.ml
@@ -1,12 +1,14 @@
 type 'a rle = One of 'a | Many of int * 'a
 
 let encode lst =
+  let make_rle count item =
+    if count = 0 then One item else Many (count + 1, item)
+  in
   let rec helper count acc lst =
     match lst with
     | x :: (y :: _ as tl) ->
         if x = y then helper (count + 1) acc tl
-        else if count = 0 then helper 0 (One x :: acc) tl
-        else helper 0 (Many (count + 1, x) :: acc) tl
+        else helper 0 (make_rle count x :: acc) tl
     | x :: [] -> if count = 0 then One x :: acc else Many (count + 1, x) :: acc
     | [] -> acc
   in
